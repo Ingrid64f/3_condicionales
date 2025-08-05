3e100a8aee0db1f2d4bafeff025baac7cc77d3a8
Ingrid64f <ingridjaramillo848@gmail.com>
2025-08-04 20:07:13 -0500

    Avanzando

---
+++ app.js
@@ -0,0 +1,19 @@
+//Ejercicio 1 de condicionales
+//Personas que pueden ingresar a la discoteca
+
+Prompt ("Dame tu nombre") 
+Prompt ("Dime tu edad")
+
+var nombre = "Mario" ;
+else if (nombre == "Mario") {
+alert ( "aun no has dicho tu nombre");
+}
+
+ var edad = "19";
+
+ if ( edad== "19") {
+alert ("Eres mayor de edad puedes ingresar a la discoteca");
+} 
+else if(edad < 18) { 
+}
+
