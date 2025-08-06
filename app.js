d34f23ca8d4f213e6e38ca4ae6e5cb882471afa6
Ingrid64f <ingridjaramillo848@gmail.com>
2025-08-05 22:55:48 -0500

    Ejercicios terminados

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
-//Ejercicio 1 de condicionales
-//Personas que pueden ingresar a la discoteca
+/* //Ejercicio 1 de condicionales-Ingreso a la discoteca
 
-Prompt ("Dame tu nombre") 
-Prompt ("Dime tu edad")
+//Datos del usuario
+let nombre = prompt("Dame tu nombre:");
+let edad= parseInt(prompt("Ingresa tu edad:"));
 
-var nombre = "Mario" ;
-else if (nombre == "Mario") {
-alert ( "aun no has dicho tu nombre");
+// Verificar la edad
+if (edad >= 18) {
+
+    // Si es mayor de edad, puede ingresar
+    mensaje = `Holis Bienvenido ${nombre}, puedes ingresar a la discoteca, disfrútalo.`;
+    
+    // Verificar acceso VIP dentro del bloque if
+    switch (nombre.toLowerCase()) {
+        case "mario":
+        case "carlos":
+            mensaje += " Además, tienes acceso al VIP.";
+            break;
+        default:
+            // No hace nada si no es Mario o Carlos
+            break;
+    }
+} else {
+    // Si es menor de edad, no puede ingresar
+    mensaje = `¡Wio,Wio,Wio! Lo siento ${nombre}, no puedes ingresar a la discoteca,eres menor.`;
 }
 
- var edad = "19";
+// Mostrar los resultado en las pantallas
+console.log(mensaje);
+alert(mensaje);  */
 
- if ( edad== "19") {
-alert ("Eres mayor de edad puedes ingresar a la discoteca");
-} 
-else if(edad < 18) { 
+ 
+
+//Ejercicio 2-Ingreso al equipo
+
+//Datos del usuario
+let estatura = prompt("Dime tu estatura en cm:");
+let velocidad= parseInt(prompt("Cual es tu velocidad en km/h:"));
+let edad = parseInt(prompt("Dime tu edad:")); 
+
+//Requisitos basicos para ser el selecionado
+if (estatura >= 170 && velocidad >= 27) {
+alert("Puedes ingresar a un equipo")
+}
+else {
+    alert("¡Oh no!,no cumples con los requisitos")
 }
 
+if (edad >= 18) {
+    alert("¡felicidades!,pasas a las divisiones mayores")
+}
+else {
+    alert("¡felicidades!, pasas a las divisones menores")
+}
+    
+console.log(mensaje);
+alert(mensaje);
+    
